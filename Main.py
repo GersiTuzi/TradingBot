9f046ff7b387e9fc5c8d94abfe488e86bf67c0a2
RoadToSiliconValley <43251223+RoadToSiliconValley@users.noreply.github.com>
2018-10-16 20:49:55 -0400

    debut api alpha vantage

--- Main.py
+++ Main.py
@@ -1,47 +1,22 @@
 
-import sys, getopt
-#bonjour
-
-def main(argv):
-	period = 10
-	pair = "BTC_XML"
-
-	try:
-		opts, args = getopt.getopt(argv,"hp:c:n:s:e:",["period=","currency=","points="])
-	except getopt.GetoptError:
-		print ("trading-bot.py -p <period length> -c <currency pair> -n <period of moving average>")
-		sys.exit(2)
-    #arguments
-	for opt, arg in opts:
-		if opt == '-h':
-			print ('trading-bot.py -p <period length> -c <currency pair> -n <period of moving average>')
-			sys.exit()
-		elif opt in ("-p", "--period"):
-			if (int(arg) in [300,900,1800,7200,14400,86400]):
-				period = arg
-			else:
-				print 'Poloniex requires periods in 300,900,1800,7200,14400, or 86400 second increments'
-				sys.exit(2)
-		elif opt in ("-c", "--currency"):
-			pair = arg
-		elif opt in ("-n", "--points"):
-			lengthOfMA = int(arg)
-		elif opt in ("-s"):
-			startTime = arg
-		elif opt in ("-e"):
-			endTime = arg
+import requests
 
 
 
-	#conn = poloniex('key goes here','key goes here') # connection au api
+API_URL = "https://www.alphavantage.co/query" 
+symbols = ['USDCAD']
 
-	if (startTime):
-		historicalData = conn.api_query("returnChartData",{"currencyPair":pair,"start":startTime,"end":endTime,"period":period})
+for symbol in symbols:
+        data = { "function": "TIME_SERIES_INTRADAY", 
+        "symbol": symbol,
+        "interval" : "30min",
+        "datatype": "json", 
+        "apikey": "BXE971HIQ45YMM8Z" }
+        response = requests.get(API_URL, data) 
+        data = response.json()
+        print(symbol)
+        a = (data['Time Series (30min)'])
+        keys = (a.keys())
+        for key in keys:
+                print(a[key]['2. high'] + " " + a[key]['5. volume'])
 
-	while True:
-
-    #implementer stratégie
-
-
-if __name__ == "__main__":
-	main(sys.argv[1:])
