bc5dea3ef2aeefc6ed0f09fa98e2c862febb96fe
RoadToSiliconValley <43251223+RoadToSiliconValley@users.noreply.github.com>
2018-10-19 22:10:27 -0400

    avancement majjjjjjjeuuuur

--- Main.py
+++ Main.py
@@ -1,38 +1,54 @@
 import requests
 
-API_URL = "https://www.alphavantage.co/query" 
-# symbols = ['USDCAD']
-#
-# for symbol in symbols:
-#         data = { "function": "TIME_SERIES_INTRADAY",
-#         "symbol": symbol,
-#         "interval" : "30min",
-#         "datatype": "json",
-#         "apikey": "BXE971HIQ45YMM8Z" }
-#         response = requests.get(API_URL, data)
-#         data = response.json()
-#         print(symbol)
-#         a = (data['Time Series (30min)'])
-#         keys = (a.keys())
-#         for key in keys:
-#                 print(a[key]['2. high'] + " " + a[key]['5. volume'])
-
+API_URL = "https://www.alphavantage.co/query"
 
 symbols = ['USDCAD']
+MagasindeKeys = []
+MagasinDesPrix = []
+tableauDeKeys = ["YGY55JABOFDU3X8Y","BXE971HIQ45YMM8Z","LU8TM1QS01RIUOL2"]
 
-for symbol in symbols:
-        data = { "function": "RSI",
-        "symbol": symbol,
-        "interval" : "30min",
-        "time_period": "60",
-        "series_type": "close",
-        "datatype": "json",
-        "apikey": "BXE971HIQ45YMM8Z" }
-        response = requests.get(API_URL, data)
-        data = response.json()
-        print(data)
-        a = (data['Technical Analysis: RSI'])
-        keys = (a.keys())
-        for key in keys:
-                print(a[key]['RSI'])
+
+def api(symbol):
+    temp1 = 0
+    temp2 = 0
+
+    data = {"function": "TIME_SERIES_INTRADAY",
+            "symbol": symbol,
+            "interval": "30min",
+            "datatype": "json",
+            "apikey": "YGY55JABOFDU3X8Y"}
+    response = requests.get(API_URL, data)
+    data = response.json()
+    a = (data['Time Series (30min)'])
+    keys = (a.keys())
+    for key in keys:
+        MagasinDesPrix.append(a[key]['2. high'])
+        temp2 += 1
+
+    data = { "function": "RSI",
+             "symbol": symbol,
+             "interval": "30min",
+             "time_period": "30",
+             "series_type": "close",
+             "datatype": "json",
+             "apikey": "YGY55JABOFDU3X8Y" }
+    response = requests.get(API_URL, data)
+    data = response.json()
+    a = (data['Technical Analysis: RSI'])
+    keys = (a.keys())
+
+    for key in keys:
+        if temp1 < temp2:
+            MagasindeKeys.append(a[key]['RSI'])
+            temp1 += 1
+
+    return MagasindeKeys, MagasinDesPrix
+
+
+tableau = api('USDJPY')
+print(tableau)
+
+
+
+
 
